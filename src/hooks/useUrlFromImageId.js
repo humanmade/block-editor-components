f9e9ef514d6b5f0196e6e505aa5b86ffe8037c0b
Joe Bailey-Roberts <joe.br@humanmade.com>
2022-10-24 15:11:45 +0100

    Add useUrlFromImageId hook

---
+++ src/hooks/useUrlFromImageId.js
@@ -0,0 +1,16 @@
+import { useSelect } from '@wordpress/data';
+
+/**
+ * Return an image url for a given image ID.
+ *
+ * @param {number} imageId - the image ID
+ * @param {?string} imageSize - the thumbail size. Defaults to 'thumbnail'.
+ * @returns {?string} - the image UL, if found.
+ */
+export default function useUrlFromImageId( imageId, imageSize = 'thumbnail' ) {
+	return useSelect( select => {
+		const media = select( 'core' ).getMedia( imageId );
+
+		return media?.media_details?.sizes?.[ imageSize ]?.source_url ?? media?.source_url;
+	}, [ imageId, imageSize ] );
+}

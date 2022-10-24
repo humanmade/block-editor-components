259609ab0fb6ed14b0891047ffd9bb3bc4ac9ce2
Thorsten Frommen <info@tfrommen.de>
2022-10-24 18:44:16 +0200

    Update useUrlFromImageId.js

--- src/hooks/useUrlFromImageId.js
+++ src/hooks/useUrlFromImageId.js
@@ -7,7 +7,7 @@ import { useSelect } from '@wordpress/data';
  * @param {string} [imageSize='thumbnail'] - Optional. Image size slug. Defaults to 'thumbnail'.
  * @returns {string} Image URL, if found.
  */
-export default function useUrlFromImageId( imageId, imageSize = 'thumbnail' ) {
+export default function useImageUrlById( imageId, imageSize = 'thumbnail' ) {
 	return useSelect(
 		( select ) => {
 			const media = select( 'core' ).getMedia( imageId );

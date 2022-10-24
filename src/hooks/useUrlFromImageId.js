0452103cee42a33837404e0ac46f15b90517f3ed
Thorsten Frommen <info@tfrommen.de>
2022-10-24 18:42:44 +0200

    Update useUrlFromImageId.js

--- src/hooks/useUrlFromImageId.js
+++ src/hooks/useUrlFromImageId.js
@@ -1,16 +1,19 @@
 import { useSelect } from '@wordpress/data';
 
 /**
- * Return an image url for a given image ID.
+ * Get the image URL for a given ID and size.
  *
- * @param {number} imageId - the image ID
- * @param {?string} imageSize - the thumbail size. Defaults to 'thumbnail'.
- * @returns {?string} - the image URL, if found.
+ * @param {number} imageId - Image ID.
+ * @param {string} [imageSize='thumbnail'] - Optional. Image size slug. Defaults to 'thumbnail'.
+ * @returns {string} Image URL, if found.
  */
 export default function useUrlFromImageId( imageId, imageSize = 'thumbnail' ) {
-	return useSelect( select => {
-		const media = select( 'core' ).getMedia( imageId );
+	return useSelect(
+		( select ) => {
+			const media = select( 'core' ).getMedia( imageId );
 
-		return media?.media_details?.sizes?.[ imageSize ]?.source_url ?? media?.source_url;
-	}, [ imageId, imageSize ] );
+			return media?.media_details?.sizes?.[ imageSize ]?.source_url ?? media?.source_url ?? '';
+		},
+		[ imageId, imageSize ]
+	);
 }

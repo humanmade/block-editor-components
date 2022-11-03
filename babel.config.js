c38918af3b9a0302349971a589a5e7abe1ca05ec
tfrommen <info@tfrommen.de>
2022-11-03 22:22:57 +0100

    Simplify Babel config

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,4 @@
-module.exports = ( api ) => {
-	api.cache.forever();
-
-	return {
-		presets: [ '@wordpress/babel-preset-default' ],
-		plugins: [ '@babel/plugin-proposal-class-properties' ],
-	};
+module.exports = {
+	presets: [ '@wordpress/babel-preset-default' ],
+	plugins: [ '@babel/plugin-proposal-class-properties' ],
 };

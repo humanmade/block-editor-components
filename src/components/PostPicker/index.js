fde7eaf0757dbebfac7078fc3865ed859fc15184
Matthew Haines-Young <matthew@humanmade.com>
2023-06-01 22:59:42 +0100

    Handle viewing current selection

--- src/components/PostPicker/index.js
+++ src/components/PostPicker/index.js
@@ -2,6 +2,8 @@ import React, { ReactNode, useCallback, useEffect } from 'react';
 
 import {
 	Button,
+	ButtonGroup,
+	TabPanel,
 	CheckboxControl,
 	Flex,
 	FlexItem,
@@ -31,6 +33,7 @@ function PostList( props ) {
 		queryArgs,
 		onChange,
 		values = [],
+		isSortable = false,
 	} = props;
 
 	const queriedPosts = useSelect( ( select ) => {
@@ -41,17 +44,49 @@ function PostList( props ) {
 		return select( 'core/data' ).isResolving( 'core', 'getEntityRecords', [ 'postType', postType, queryArgs ] );
 	} );
 
-	return useMemo( () =>
-		(
-			<div style={ {
-				height: 400,
-				maxHeight: '100%',
-				overflow: 'scroll',
-				paddingLeft: 4,
-				marginLeft: -4,
-			} }>
-				{
-					( isResolving && <Spinner/> )
+	const reorderUp = ( postId ) => {
+		const index = values.indexOf( postId );
+
+		// Item not found or already at the first position
+		if ( index === -1 || index === 0 ) {
+			return;
+		}
+
+		onChange( [
+			...values.slice( 0, index - 1 ),
+			values[ index ],
+			values[ index - 1 ],
+			...values.slice( index + 1 ),
+		] );
+	};
+
+	const reorderDown = ( postId ) => {
+		const index = values.indexOf( postId );
+
+		// Item not found or already at the last position
+		if ( index === -1 || index === values.length - 1 ) {
+			return;
+		}
+
+		onChange( [
+			...values.slice( 0, index ),
+			values[ index + 1 ],
+			values[ index ],
+			...values.slice( index + 2 ),
+		] );
+	};
+
+	return (
+		<div style={ {
+			// Overflow at top.
+			marginTop: -24,
+			paddingTop: 24,
+			// Offset to display checkbox focus outline.
+			paddingLeft: 4,
+			marginLeft: -4,
+		} }>
+			{
+				( isResolving && <Spinner /> )
 					|| ( queriedPosts.length < 1 &&
 						<Notice
 							isDismissible={ false }
@@ -61,43 +96,67 @@ function PostList( props ) {
 					)
 					|| (
 						queriedPosts.map( post => (
-							<CheckboxControl
-								key={ post.id }
-								checked={ values.includes( post.id ) }
-								label={ post.title?.rendered || __( '(No title)', 'block-editor-components' ) }
-								onChange={ ( checked ) => {
-									if ( checked ) {
-										onChange( [ ...values, post.id ] );
-									} else {
-										onChange( values.filter( value => ( value !== post.id ) ) );
-									}
+							<div
+								style={ {
+									display: 'grid',
+									gridTemplateColumns: '1fr auto',
+									marginRight: -2,
+									paddingRight: 2,
 								} }
-							/>
+							>
+								<CheckboxControl
+									key={ post.id }
+									checked={ values.includes( post.id ) }
+									label={ post.title?.rendered || __( '(No title)', 'block-editor-components' ) }
+									onChange={ ( checked ) => {
+										if ( checked ) {
+											onChange( [ ...values, post.id ] );
+										} else {
+											onChange( values.filter( value => ( value !== post.id ) ) );
+										}
+									} }
+								/>
+								{ isSortable && (
+									<ButtonGroup>
+										<Button
+											icon={ 'arrow-up-alt2' }
+											iconSize={ 12 }
+											isSmall
+											label={ __( 'Move up', 'block-editor-components' ) }
+											variant="secondary"
+											onClick={ () => reorderUp( post.id ) }
+										/>
+										<Button
+											icon={ 'arrow-down-alt2' }
+											iconSize={ 12 }
+											isSmall
+											label={ __( 'Move down', 'block-editor-components' ) }
+											variant="secondary"
+											onClick={ () => reorderDown( post.id ) }
+										/>
+									</ButtonGroup>
+								) }
+							</div>
 						) )
 					)
-				}
-			</div>
-		),
-	[ isResolving, onChange, queriedPosts, values ]
+			}
+		</div>
 	);
 }
 
 /**
- * Component allowing the selection of one or more posts
+ * Post Picker Browse Post Panel.
  *
- * @param {object} props Component props
+ * @param props
  * @returns {ReactNode} Component
  */
-function PostPicker( props ) {
+function BrowsePanel( props ) {
 	const {
-		title = __( 'Select posts', 'block_editor_components' ),
-		postType = 'post',
-		taxonomies = [],
-		values,
+		postType,
 		onChange,
+		values,
+		taxonomies,
 	} = props;
-
-	const [ modalOpen, setModalOpen ] = useState( false );
 
 	const [ search, setSearch ] = useState();
 
@@ -144,6 +203,63 @@ function PostPicker( props ) {
 	};
 
 	return (
+		<Flex
+			align="flex-start" s
+			style={ { gap: 24 } }
+		>
+			<FlexItem
+				style={ {
+					width: '35%',
+				} }
+			>
+				<SearchControl
+					label="Search Posts"
+					style={ { marginBottom: 24 } }
+					value={ search }
+					onChange={ ( text ) => setSearch( text ) }
+				/>
+				{ taxonomies.map( taxonomy => {
+					const taxObject = taxObjects.find( t => t && t.slug === taxonomy );
+
+					return taxObject ? (
+						<TermSelector
+							taxonomy={ taxonomy }
+							value={ taxQueries[ taxObject.rest_base ] }
+							onChange={ terms => updateTaxQueryState( taxonomy, terms ) }
+						/>
+					) : null;
+				} ) }
+			</FlexItem>
+			<FlexItem style={ { width: '65%' } }>
+				<PostList
+					postType={ postType }
+					queryArgs={ queryArgs }
+					values={ values }
+					onChange={ onChange }
+				/>
+			</FlexItem>
+		</Flex>
+	);
+}
+
+/**
+ * Component allowing the selection of one or more posts
+ *
+ * @param {object} props Component props
+ * @returns {ReactNode} Component
+ */
+function PostPicker( props ) {
+	const {
+		title = __( 'Select posts', 'block_editor_components' ),
+		postType = 'post',
+		taxonomies = [],
+		values,
+		onChange,
+	} = props;
+
+	const [ modalOpen, setModalOpen ] = useState( false );
+
+	return (
 		<>
 			<Button
 				variant="primary"
@@ -151,48 +267,63 @@ function PostPicker( props ) {
 			>
 				{ title }
 			</Button>
-			{
-				modalOpen &&
-				<Modal style={ {
-					width: '720px',
-					maxWidth: '100%',
-				} }
-				title={ title }
-				onRequestClose={ () => setModalOpen( false ) }
+			{ modalOpen && (
+				<Modal
+					style={ {
+						width: '800px',
+						maxWidth: '100%',
+					} }
+					title={ title }
+					onRequestClose={ () => setModalOpen( false ) }
 				>
-					<Flex align="flex-start">
-						<FlexItem style={ {
-							width: '50%',
-						} }>
-							<SearchControl
-								label="Search Posts"
-								style={ { marginBottom: 24 } }
-								value={ search }
-								onChange={ ( text ) => setSearch( text ) }
-							/>
-							{ taxonomies.map( taxonomy => {
-								const taxObject = taxObjects.find( t => t && t.slug === taxonomy );
+					<div style={ { marginTop: -16 } }>
+						<TabPanel
+							tabs={ [
+								{
+									name: 'browse',
+									title: __( 'Browse Posts', 'skyscanner' ),
+								},
+								{
+									name: 'selection',
+									title: __( 'Current Selection', 'skyscanner' ),
+								},
+							] }
+						>
+							{ tabPanel => (
+								<div style={ {
+									marginTop: 'calc( var(--wp-admin-border-width-focus) * -1 )',
+									borderStyle: 'none',
+									borderTop: 'var( --wp-admin-border-width-focus ) solid #ddd',
+									paddingTop: 24,
+								} }>
+									{ tabPanel.name === 'browse' && (
+										<BrowsePanel
+											postType={ postType }
+											taxonomies={ taxonomies }
+											values={ values }
+											onChange={ onChange }
+										/>
+									) }
 
-								return taxObject ? (
-									<TermSelector
-										taxonomy={ taxonomy }
-										value={ taxQueries[ taxObject.rest_base ] }
-										onChange={ terms => updateTaxQueryState( taxonomy, terms ) }
-									/>
-								) : null;
-							} ) }
-						</FlexItem>
-						<FlexItem style={ { width: '50%' } }>
-							<PostList
-								postType={ postType }
-								queryArgs={ queryArgs }
-								values={ values }
-								onChange={ onChange }
-							/>
-						</FlexItem>
-					</Flex>
+									{ tabPanel.name === 'selection' && (
+										<PostList
+											isSortable
+											postType={ postType }
+											queryArgs={ {
+												include: values,
+												orderby: 'include',
+												per_page: values.length,
+											} }
+											values={ values }
+											onChange={ onChange }
+										/>
+									) }
+								</div>
+							) }
+						</TabPanel>
+					</div>
 				</Modal>
-			}
+			) }
 		</>
 	);
 }

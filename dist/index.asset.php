96dfd659d7156b2bcedba6a7949018d536ba9b35
Matthew Haines-Young <matthew@humanmade.com>
2023-06-02 14:36:25 +0100

    0.5.0-beta.0

---
+++ dist/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-dom', 'wp-element', 'wp-html-entities', 'wp-i18n', 'wp-server-side-render', 'wp-url'), 'version' => '0fb1f5094ff6bed4c7e6');

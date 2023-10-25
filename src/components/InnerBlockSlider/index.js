2c19211261720c663363bd89bafe7d8c0c0fcd13
Matthew Haines-Young <matthew@humanmade.com>
2023-10-25 17:17:27 +0100

    Update docs

--- src/components/InnerBlockSlider/index.js
+++ src/components/InnerBlockSlider/index.js
@@ -5,6 +5,9 @@ import InnerBlockSlider from './inner-block-slider.js';
 /**
  * InnerBlockSlider container component.
  *
+ * This container component is recommended for typical usage, as it handles state management.
+ * However for further info on supported props, refer to the InnerBlockSlider component, as all props are passed through.
+ *
  * @param {object} props      Component props.
  * @returns {React.ReactNode} Component.
  */
